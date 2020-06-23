d00cc9cbdb511aaee2c7756ed3d4fd69941cac37
alcantarar <ryan.alcantara@colorado.edu>
2020-06-23 12:07:30 -0600

    add main_script.m

---
+++ main_script.m
@@ -0,0 +1,18 @@
+%   ASB TUTORIAL MASTER SCRIPT
+%
+%   Each of the scripts below has a bug that needs to be fixed!
+%
+
+hold on
+
+script_1 
+
+script_2 
+
+script_3 
+
+script_4 
+
+script_5
+
+disp('great job!')
