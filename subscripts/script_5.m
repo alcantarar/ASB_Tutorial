be6d55712f8464be1f92846a3785dc9e01baac35
ross-wilkinson <ross.wilkinson@icloud.com>
2020-07-08 21:06:02 -0600

    Comment out solutions

--- subscripts/script_5.m
+++ subscripts/script_5.m
@@ -1,10 +1,10 @@
 %script_5
 
-% %today's date
-% date_text = date;
-% year = date_text(end-4:end);
-% %plot text
-% text(2.5,-3.5, year, 'FontSize', 32, 'FontWeight', 'bold')
+%today's date
+date_text = date;
+year = date_text(end-4:end);
+%plot text
+text(2.5,-3.5, year, 'FontSize', 32, 'FontWeight', 'bold')
 
 %% SOLUTION: Breakout 1 
 % This section has the fixed code if you are struggling to debug!
@@ -17,7 +17,7 @@
 
 %% SOLUTION: Breakout 2
 
-%today's date
-year = date_text(end-3:end);
-%plot text
-text(2.5,-3.5, ['Virtual ' year], 'FontSize', 24, 'FontWeight', 'bold') % Include 'Virtual ' before 2020
+% %today's date
+% year = date_text(end-3:end);
+% %plot text
+% text(2.5,-3.5, ['Virtual ' year], 'FontSize', 24, 'FontWeight', 'bold') % Include 'Virtual ' before 2020
