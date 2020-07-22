69025e9f6013bce0e70cfca282fbe35845966f90
alcantarar <ryan.alcantara@colorado.edu>
2020-07-22 14:18:27 -0600

    rm script2 solution and comment out solutions in script2

--- subscripts/script_2.m
+++ subscripts/script_2.m
@@ -5,3 +5,19 @@ data = csvread('data/leaf.cvs');
 %plot data
 l = patch(data(:,1), data(:,2), 'green');
 l.LineWidth = 2;
+
+%% SOLUTION: Breakout 1
+
+%read in data
+%data = csvread('leaf.csv'); % correct filename
+%plot data
+%l = patch(data(:,1), data(:,2), 'green');
+%l.LineWidth = 2;
+
+%% SOLUTION: Breakout 2
+
+%read in data
+%data = csvread('leaf.csv');
+%plot data
+%l = patch(data(:,1), data(:,2), [0.5686 0.5882 0.3765]);
+%l.LineWidth = 1; % change to 1
