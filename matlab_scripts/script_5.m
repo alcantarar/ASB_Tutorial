9e5bb1630eb524b8e96bfebaf2b5bf1a3109c4db
Ryan Alcantara <ryan.alcantara@colorado.edu>
2020-07-28 11:07:49 -0600

    change matlab file/path names, fix xlim/ylim issue with script_5.m

--- matlab_scripts/script_5.m
+++ matlab_scripts/script_5.m
@@ -8,8 +8,8 @@ date_text = date;
 year = date_text(end-3:end);
 %plot text
 text(2.5,-3.5, year, 'FontSize', 24, 'FontWeight', 'bold')
-ylim([-2,12])
-xlim([-8,10])
+xlim([-2,12])
+ylim([-8,10])
 
 %% SOLUTION: Breakout 1
 
@@ -18,5 +18,5 @@ xlim([-8,10])
 % year = date_text(end-3:end);
 %plot text
 % text(2.5,-3.5, 'Reviewer 2 is the best', 'FontSize', 24, 'FontWeight', 'bold', 'Color', 'g') % per reviewer comments
-% ylim([-2,12])
-% xlim([-8,10])
+% xlim([-2,12])
+% ylim([-8,10])
