6c86ad1906aea2a9519c9f2c61941e975d64a8b7
MATHEW SUNIL VARRE <varre@unlv.nevada.edu>
2020-08-04 11:17:08 -0700

    Changed color to brown

--- matlab_scripts/script_2.m
+++ matlab_scripts/script_2.m
@@ -6,7 +6,7 @@
 %read in data
 data = csvread('data/leaf.csv');
 %plot data
-l = patch(data(:,1), data(:,2), 'green');
+l = patch(data(:,1), data(:,2), [0.792, 0.647, 0.278]);
 l.LineWidth = 2;
 
 %% SOLUTION: Breakout 1
