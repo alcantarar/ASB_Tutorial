4024933f74fb2214983ca34f765e25966f31fbc9
FranzBiomech <jrfranz@email.unc.edu>
2020-08-04 14:04:58 -0400

    Script 1 Changed to make a purple peach

--- matlab_scripts/script_1.m
+++ matlab_scripts/script_1.m
@@ -12,7 +12,8 @@ p.LineWidth = 2;
 %% SOLUTION: Breakout 1
 
 %read in data
-% data = csvread('data/peach.csv');
+data = csvread('data/peach.csv');
 %plot data
-% p = patch(data(:,1), data(:,2), [0.506, 0.0, 0.937]); % per reviewer comments
-% p.LineWidth = 2;
+p = patch(data(:,1), data(:,2), [0.506, 0.0, 0.937]); % per reviewer comments
+p.LineWidth = 2;
+
