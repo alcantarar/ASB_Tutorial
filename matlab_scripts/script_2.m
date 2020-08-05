e3cfd668f90dc8477bb64475c7bd20f35e21db7f
drapuan <drapuan95@gmail.com>
2020-08-05 15:20:56 -0700

    Changed Script 2 to meet reviewer request

--- matlab_scripts/script_2.m
+++ matlab_scripts/script_2.m
@@ -6,8 +6,8 @@
 %read in data
 data = csvread('data/leaf.csv');
 %plot data
-l = patch(data(:,1), data(:,2), 'green');
-l.LineWidth = 2;
+l = patch(data(:,1), data(:,2), [0.792, 0.647, 0.278]);
+l.LineWidth = 1;
 
 %% SOLUTION: Breakout 1
 
