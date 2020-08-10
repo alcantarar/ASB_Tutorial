056a9ef13529f94b15158d940fdb66f22885a07b
Sepehr Ramezani <ramezani.dan@gmail.com>
2020-08-10 11:36:30 -0400

    Change logo color to red

--- matlab_scripts/script_4.m
+++ matlab_scripts/script_4.m
@@ -4,13 +4,13 @@
 %
 
 %read in data
-data = csvread('data/ASB.csv');
+% data = csvread('data/ASB.csv');
 %plot data
-plot(data(:,1), data(:,2), 'k-', 'LineWidth',7);
+% plot(data(:,1), data(:,2), 'k-', 'LineWidth',7);
 
 %% SOLUTION: Breakout 1
 
 %read in data
-% data = csvread('data/ASB.csv');
+data = csvread('data/ASB.csv');
 %plot data
-% plot(data(:,1), data(:,2), 'r-', 'LineWidth',7); % change to red per reviewer comments
+plot(data(:,1), data(:,2), 'r-', 'LineWidth',7); % change to red per reviewer comments
