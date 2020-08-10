320a418a36b4edb1643ab1e97084f2a7948e9882
Sepehr Ramezani <ramezani.dan@gmail.com>
2020-08-10 11:35:22 -0400

    Producing peach stem and halo

--- matlab_scripts/script_3.m
+++ matlab_scripts/script_3.m
@@ -4,21 +4,21 @@
 %
 
 %read in data
+% data = csvread('data/outline.csv');
+% %remove missing data
+% data(data == -999) = NaN;
+% %plot data
+% hold on
+% plot(data(:,1), data(:,2), 'k-', 'LineWidth',3);
+% plot(data(:,3), data(:,4), 'k-', 'LineWidth',3);
+
+%% SOLUTION: Breakout 1
+
+%read in data
 data = csvread('data/outline.csv');
 %remove missing data
 data(data == -999) = NaN;
 %plot data
 hold on
-plot(data(:,1), data(:,2), 'k-', 'LineWidth',3);
-plot(data(:,3), data(:,4), 'k-', 'LineWidth',3);
-
-%% SOLUTION: Breakout 1
-
-%read in data
-% data = csvread('data/outline.csv');
-%remove missing data
-% data(data == -999) = NaN;
-%plot data
-% hold on
-% plot(data(:,1), data(:,2), 'k-', 'LineWidth',10); % change linewidth to 10 per reviewer comments
-% plot(data(:,3), data(:,4), 'k-', 'LineWidth',10); % change linewidth to 10 per reviewer comments
+plot(data(:,1), data(:,2), 'k-', 'LineWidth',10); % change linewidth to 10 per reviewer comments
+plot(data(:,3), data(:,4), 'k-', 'LineWidth',10); % change linewidth to 10 per reviewer comments
